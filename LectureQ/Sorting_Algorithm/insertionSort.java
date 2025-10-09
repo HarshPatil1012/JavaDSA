41eae3a393f7705220a34892e0e05e81b65e3bf7
Harsh Patil <harshp.101203@gmail.com>
2025-10-09 14:54:32 +0530

    Add : Sorting Questions

---
+++ LectureQ/Sorting_Algorithm/insertionSort.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class insertionSort {
+
+    // --> time complexity = O(n^2) in worst case and O(n) in best case (when array is already sorted)
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {  // logic starts from 1st index bcz 0th index is already sorted
+            int curr = arr[i];
+            int prev = i - 1;  // Assuming 0 to i-1 is sorted
+
+            // finding out the correct position to insert current element
+            while (prev >= 0 && arr[prev] > curr) {
+            //while (prev >= 0 && arr[prev] < curr) { // for descending order
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+
+            // insertion
+            arr[prev + 1] = curr; // placing current element at its correct position
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        insertionSort(arr);
+        printArr(arr);
+    }
+
+}
---
+++ LectureQ/Sorting_Algorithm/selectionSort.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class selectionSort {
+
+    // --> time complexity = O(n^2) in all cases (best, average, worst)
+    
+    public static void selectionSort(int[] arr) {
+        for(int i=0; i<arr.length; i++){
+            int minPos = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[minPos] > arr[j]){
+        //      if(arr[minPos] < arr[j]){  // for descending order bcz now it will find maximum value and place it at first
+                    minPos = j;
+                }
+            }
+            // swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();  
+        }
+        selectionSort(arr);
+        printArr(arr);
+    }
+
+}
