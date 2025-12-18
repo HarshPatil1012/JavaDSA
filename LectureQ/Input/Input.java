523c6ca6f02f88b8db9a61650d75fe8a19fae136
Harsh Patil <harshp.101203@gmail.com>
2025-12-18 12:02:37 +0530

    Update: minor fixes

---
+++ LectureQ/Input/Input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Input{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Please enter a number : ");
+    int number = sc.nextInt();
+        System.out.println("your number is : " + number);
+
+    }
+}
